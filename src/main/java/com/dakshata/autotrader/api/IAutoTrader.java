27d5ff2f6896769d9ede06504c46210957227f71
PRITESH <mhatre.pritesh@gmail.com>
2020-06-22 15:43:22 +0530

    Place & cancel order functions added

--- src/main/java/com/dakshata/autotrader/api/IAutoTrader.java
+++ src/main/java/com/dakshata/autotrader/api/IAutoTrader.java
@@ -5,10 +5,14 @@ package com.dakshata.autotrader.api;
 
 import java.util.Set;
 
+import com.dakshata.constants.trading.OrderType;
+import com.dakshata.constants.trading.ProductType;
+import com.dakshata.constants.trading.TradeType;
 import com.dakshata.data.model.common.IOperationResponse;
 import com.dakshata.trading.model.platform.PlatformMargin;
 import com.dakshata.trading.model.platform.PlatformOrder;
 import com.dakshata.trading.model.platform.PlatformPosition;
+import com.dakshata.trading.model.portfolio.Order;
 
 /**
  * AutoTrader API instance.
@@ -24,6 +28,84 @@ public interface IAutoTrader {
 	 * @return live pseudo accounts
 	 */
 	IOperationResponse<Set<String>> fetchLivePseudoAccounts();
+
+	/**
+	 * Places an order. For more information, please see <a href=
+	 * "https://stocksdeveloper.in/documentation/api/place-advanced-order/">api
+	 * docs</a>.
+	 *
+	 * @param order order object
+	 * @return the order id given by your stock broker
+	 */
+	IOperationResponse<String> placeOrder(Order order);
+
+	/**
+	 * Places a regular order. For more information, please see <a href=
+	 * "https://stocksdeveloper.in/documentation/api/place-regular-order/">api
+	 * docs</a>.
+	 *
+	 * @param pseudoAccount pseudo account
+	 * @param exchange      exchange
+	 * @param symbol        symbol
+	 * @param tradeType     trade type
+	 * @param orderType     order type
+	 * @param productType   product type
+	 * @param quantity      quantity
+	 * @param price         price
+	 * @param triggerPrice  trigger price
+	 * @return the order id given by your stock broker
+	 */
+	IOperationResponse<String> placeRegularOrder(final String pseudoAccount, String exchange, String symbol,
+			TradeType tradeType, OrderType orderType, ProductType productType, int quantity, float price,
+			float triggerPrice);
+
+	/**
+	 * Places a bracket order. For more information, please see <a href=
+	 * "https://stocksdeveloper.in/documentation/api/place-bracket-order/">api
+	 * docs</a>.
+	 *
+	 * @param pseudoAccount    pseudo account
+	 * @param exchange         exchange
+	 * @param symbol           symbol
+	 * @param tradeType        trade type
+	 * @param orderType        order type
+	 * @param quantity         quantity
+	 * @param price            price
+	 * @param triggerPrice     trigger price
+	 * @param target           target
+	 * @param stoploss         stoploss
+	 * @param trailingStoploss trailing stoploss
+	 * @return the order id given by your stock broker
+	 */
+	IOperationResponse<String> placeBracketOrder(String pseudoAccount, String exchange, String symbol,
+			TradeType tradeType, OrderType orderType, int quantity, float price, float triggerPrice, float target,
+			float stoploss, float trailingStoploss);
+
+	/**
+	 * Places a cover order. For more information, please see <a href=
+	 * "https://stocksdeveloper.in/documentation/api/place-cover-order/">api
+	 * docs</a>.
+	 *
+	 * @param pseudoAccount pseudo account
+	 * @param exchange      exchange
+	 * @param symbol        symbol
+	 * @param tradeType     trade type
+	 * @param orderType     order type
+	 * @param quantity      quantity
+	 * @param price         price
+	 * @param triggerPrice  trigger price
+	 * @return the order id given by your stock broker
+	 */
+	IOperationResponse<String> placeCoverOrder(String pseudoAccount, String exchange, String symbol,
+			TradeType tradeType, OrderType orderType, int quantity, float price, float triggerPrice);
+
+	/**
+	 *
+	 * @param pseudoAccount pseudo account
+	 * @param platformId    platform id (id given to order by trading platform)
+	 * @return <code>true</code> on success, <code>false</code> otherwise
+	 */
+	IOperationResponse<Boolean> cancelOrderByPlatformId(String pseudoAccount, String platformId);
 
 	/**
 	 * This function executes a given command. The command can be operations like
