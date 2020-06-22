4edcd323f15dcc8821edb8a4a9cb8065a9e73233
PRITESH <mhatre.pritesh@gmail.com>
2020-06-22 17:28:53 +0530

    Cancel and place order methods

--- src/main/java/com/dakshata/autotrader/api/AutoTrader.java
+++ src/main/java/com/dakshata/autotrader/api/AutoTrader.java
@@ -6,13 +6,19 @@ package com.dakshata.autotrader.api;
 import static com.dakshata.constants.autotrader.IAutoTrader.API_KEY_HEADER;
 import static com.dakshata.tools.internet.HttpStatus.toTextDefault;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
+import com.dakshata.constants.trading.OrderType;
+import com.dakshata.constants.trading.ProductType;
+import com.dakshata.constants.trading.TradeType;
 import com.dakshata.data.model.common.IOperationResponse;
 import com.dakshata.data.model.common.OperationResponse;
 import com.dakshata.trading.model.platform.PlatformMargin;
 import com.dakshata.trading.model.platform.PlatformOrder;
 import com.dakshata.trading.model.platform.PlatformPosition;
+import com.dakshata.trading.model.portfolio.Order;
 
 import kong.unirest.Config;
 import kong.unirest.GenericType;
@@ -22,7 +28,8 @@ import kong.unirest.UnirestInstance;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Implementation of AutoTrade API functions.
+ * Implementation of AutoTrade API functions. Only create one instance of this
+ * class per API key.
  *
  * @author PRITESH
  *
@@ -42,6 +49,10 @@ public class AutoTrader implements IAutoTrader {
 
 	private final String readPlatformOrdersUrl, readPlatformPositionsUrl, readPlatformMarginsUrl;
 
+	private final String placeOrderUrl, placeRegularOrderUrl, placeCoverOrderUrl, placeBracketOrderUrl;
+
+	private final String cancelOrderByPlatformIdUrl;
+
 	/**
 	 * Initialize the AutoTrader API with your private API key.
 	 *
@@ -55,6 +66,11 @@ public class AutoTrader implements IAutoTrader {
 		this.readPlatformOrdersUrl = serviceUrl + TRADING_URI + "/readPlatformOrders";
 		this.readPlatformPositionsUrl = serviceUrl + TRADING_URI + "/readPlatformPositions";
 		this.readPlatformMarginsUrl = serviceUrl + TRADING_URI + "/readPlatformMargins";
+		this.placeOrderUrl = serviceUrl + TRADING_URI + "/placeOrder";
+		this.placeRegularOrderUrl = serviceUrl + TRADING_URI + "/placeRegularOrder";
+		this.placeCoverOrderUrl = serviceUrl + TRADING_URI + "/placeCoverOrder";
+		this.placeBracketOrderUrl = serviceUrl + TRADING_URI + "/placeBracketOrder";
+		this.cancelOrderByPlatformIdUrl = serviceUrl + TRADING_URI + "/cancelOrderByPlatformId";
 		this.livePseudoAccountsUrl = serviceUrl + ACCOUNT_URI + "/fetchLivePseudoAccounts";
 	}
 
@@ -74,6 +90,90 @@ public class AutoTrader implements IAutoTrader {
 	public IOperationResponse<? extends Object> execute(final String command) {
 		final HttpResponse<OperationResponse<? extends Object>> response = this.client.post(this.commandUrl)
 				.field("command", command).asObject(new GenericType<OperationResponse<? extends Object>>() {
+				});
+		if (response.getStatus() != 200) {
+			return this.processHttpError(response);
+		}
+
+		return response.getBody();
+	}
+
+	@Override
+	public IOperationResponse<String> placeOrder(final Order order) {
+		final HttpResponse<OperationResponse<String>> response = this.client.post(this.placeOrderUrl).body(order)
+				.asObject(new GenericType<OperationResponse<String>>() {
+				});
+		if (response.getStatus() != 200) {
+			return this.processHttpError(response);
+		}
+
+		return response.getBody();
+	}
+
+	@Override
+	public IOperationResponse<String> placeRegularOrder(final String pseudoAccount, final String exchange,
+			final String symbol, final TradeType tradeType, final OrderType orderType, final ProductType productType,
+			final int quantity, final float price, final float triggerPrice) {
+		final Map<String, Object> params = new HashMap<>();
+		params.put("pseudoAccount", pseudoAccount);
+		params.put("exchange", exchange);
+		params.put("symbol", symbol);
+		params.put("tradeType", tradeType);
+		params.put("orderType", orderType);
+		params.put("productType", productType);
+		params.put("quantity", quantity);
+		params.put("price", price);
+		params.put("triggerPrice", triggerPrice);
+
+		return this.postOrder(this.placeRegularOrderUrl, params);
+	}
+
+	@Override
+	public IOperationResponse<String> placeBracketOrder(final String pseudoAccount, final String exchange,
+			final String symbol, final TradeType tradeType, final OrderType orderType, final int quantity,
+			final float price, final float triggerPrice, final float target, final float stoploss,
+			final float trailingStoploss) {
+		final Map<String, Object> params = new HashMap<>();
+		params.put("pseudoAccount", pseudoAccount);
+		params.put("exchange", exchange);
+		params.put("symbol", symbol);
+		params.put("tradeType", tradeType);
+		params.put("orderType", orderType);
+		params.put("quantity", quantity);
+		params.put("price", price);
+		params.put("triggerPrice", triggerPrice);
+		params.put("target", target);
+		params.put("stoploss", stoploss);
+		params.put("trailingStoploss", trailingStoploss);
+
+		return this.postOrder(this.placeBracketOrderUrl, params);
+	}
+
+	@Override
+	public IOperationResponse<String> placeCoverOrder(final String pseudoAccount, final String exchange,
+			final String symbol, final TradeType tradeType, final OrderType orderType, final int quantity,
+			final float price, final float triggerPrice) {
+		final Map<String, Object> params = new HashMap<>();
+		params.put("pseudoAccount", pseudoAccount);
+		params.put("exchange", exchange);
+		params.put("symbol", symbol);
+		params.put("tradeType", tradeType);
+		params.put("orderType", orderType);
+		params.put("quantity", quantity);
+		params.put("price", price);
+		params.put("triggerPrice", triggerPrice);
+
+		return this.postOrder(this.placeCoverOrderUrl, params);
+	}
+
+	@Override
+	public IOperationResponse<Boolean> cancelOrderByPlatformId(final String pseudoAccount, final String platformId) {
+		final Map<String, Object> params = new HashMap<>();
+		params.put("pseudoAccount", pseudoAccount);
+		params.put("platformId", platformId);
+
+		final HttpResponse<OperationResponse<Boolean>> response = this.client.post(this.cancelOrderByPlatformIdUrl)
+				.fields(params).asObject(new GenericType<OperationResponse<Boolean>>() {
 				});
 		if (response.getStatus() != 200) {
 			return this.processHttpError(response);
@@ -126,6 +226,14 @@ public class AutoTrader implements IAutoTrader {
 		this.init(apiKey);
 	}
 
+	/**
+	 * Graceful shutdown. Call when your application is about to terminate.
+	 */
+	@Override
+	public void shutdown() {
+		this.shutdownClient();
+	}
+
 	private void init(final String apiKey) {
 		this.shutdownClient();
 		final Config config = new Config();
@@ -145,12 +253,15 @@ public class AutoTrader implements IAutoTrader {
 		}
 	}
 
-	/**
-	 * Graceful shutdown. Call when your application is about to terminate.
-	 */
-	@Override
-	public void shutdown() {
-		this.shutdownClient();
+	private IOperationResponse<String> postOrder(final String url, final Map<String, Object> params) {
+		final HttpResponse<OperationResponse<String>> response = this.client.post(url).fields(params)
+				.asObject(new GenericType<OperationResponse<String>>() {
+				});
+		if (response.getStatus() != 200) {
+			return this.processHttpError(response);
+		}
+
+		return response.getBody();
 	}
 
 	private final <T> IOperationResponse<T> processHttpError(final HttpResponse<?> response) {
