a1a863c4106803d3bbb7ba682506335259ac9d1e
PRITESH <mhatre.pritesh@gmail.com>
2020-12-11 11:10:18 +0530

    Added more test cases for live market run

--- src/test/java/com/dakshata/autotrader/api/TestAutoTraderLiveMarket.java
+++ src/test/java/com/dakshata/autotrader/api/TestAutoTraderLiveMarket.java
@@ -3,14 +3,18 @@
  */
 package com.dakshata.autotrader.api;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Set;
+import java.util.function.Function;
+
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 import com.dakshata.constants.trading.OrderType;
+import com.dakshata.constants.trading.PositionCategory;
+import com.dakshata.constants.trading.PositionType;
 import com.dakshata.constants.trading.ProductType;
 import com.dakshata.constants.trading.TradeType;
 import com.dakshata.data.model.common.IOperationResponse;
@@ -30,30 +34,85 @@ public class TestAutoTraderLiveMarket {
 	// AutoTrader.createInstance("75bf216e-a830-4ed1-a04f-fb42f330415c",
 	// "https://stocksdeveloper.in:9017");
 
-	private static final String ACC = "FA8768";
+	private static final String[] ACC = { "FA8768", "159401", "DN4048", "MM01515" };
+
+	private static final float livePrice = 272.65f;
 
 	@Tag("skip")
-	@Test
+	// @Test
 	public void testCancelOrder() throws InterruptedException {
-		final IOperationResponse<String> response = AT.placeRegularOrder(ACC, "NSE", "SBIN", TradeType.BUY,
-				OrderType.LIMIT, ProductType.INTRADAY, 1, 188.55f, 0f);
-		String orderId = null;
-		if (response.success()) {
-			orderId = response.getResult();
+		for (final String acc : ACC) {
+			System.out.println("Placing limit order in: " + acc);
+			final IOperationResponse<String> response = AT.placeRegularOrder(acc, "NSE", "SBIN", TradeType.BUY,
+					OrderType.LIMIT, ProductType.INTRADAY, 1, livePrice - 1.8f, 0f);
+			assertNotNull(response);
+			assertTrue(response.success());
+			final String orderId = response.getResult();
+			assertNotNull(orderId);
+
+			Thread.sleep(2000);
+
+			System.out.println("Cancelling limit order in: " + acc);
+			final IOperationResponse<Boolean> cancelResponse = AT.cancelOrderByPlatformId(acc, orderId);
+			assertNotNull(cancelResponse);
+			assertTrue(cancelResponse.success());
+			assertNotNull(cancelResponse.getResult());
+			assertTrue(cancelResponse.getResult());
 		}
-		assertNotNull(response);
-		assertNull(response.getMessage());
-		assertTrue(response.success());
-		assertNotNull(orderId);
+	}
 
-		Thread.sleep(2000);
+	@Tag("skip")
+	// @Test
+	public void testSquareOffPosition() throws InterruptedException {
+		for (final String acc : ACC) {
+			System.out.println("Placing market order in: " + acc);
+			final IOperationResponse<String> response = AT.placeRegularOrder(acc, "NSE", "SBIN", TradeType.BUY,
+					OrderType.MARKET, ProductType.INTRADAY, 1, 0f, 0f);
+			assertNotNull(response);
+			assertTrue(response.success());
+			final String orderId = response.getResult();
+			assertNotNull(orderId);
 
-		final IOperationResponse<Boolean> cancelResponse = AT.cancelOrderByPlatformId(ACC, orderId);
-		assertNotNull(cancelResponse);
-		assertTrue(cancelResponse.success());
-		assertNotNull(cancelResponse.getResult());
-		assertTrue(cancelResponse.getResult());
+			Thread.sleep(2000);
 
+			System.out.println("Squaring off position in: " + acc);
+			final IOperationResponse<Boolean> sqOffResponse = AT.squareOffPosition(acc, PositionCategory.NET,
+					PositionType.MIS, "NSE", "SBIN");
+			assertNotNull(sqOffResponse);
+			assertTrue(sqOffResponse.success());
+			assertNotNull(sqOffResponse.getResult());
+			assertTrue(sqOffResponse.getResult());
+		}
+	}
+
+	@Tag("skip")
+	// @Test
+	public void testReadOrders() {
+		this.testRead("orders", AT::readPlatformOrders);
+	}
+
+	@Tag("skip")
+	// @Test
+	public void testReadMargins() {
+		this.testRead("margins", AT::readPlatformMargins);
+	}
+
+	@Tag("skip")
+	// @Test
+	public void testReadPositions() {
+		this.testRead("positions", AT::readPlatformPositions);
+	}
+
+	public <T> void testRead(final String type, final Function<String, IOperationResponse<Set<T>>> reader) {
+		for (final String acc : ACC) {
+			System.out.println("Reading " + type + " from: " + acc);
+			final IOperationResponse<Set<T>> response = reader.apply(acc);
+			assertNotNull(response);
+			assertTrue(response.success());
+			final Set<T> part = response.getResult();
+			assertNotNull(part);
+			assertNotEquals(0, part.size());
+		}
 	}
 
 }
